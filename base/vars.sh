5e87166e8e6df6999ff02410dc59ceb3f722b268
michaelhaessig <michael.haessig@gmail.com>
2017-07-01 23:47:20 +0200

    base vars.sh script debug vars on/off fix

--- base/vars.sh
+++ base/vars.sh
@@ -25,18 +25,19 @@ if [ "$WORDPRESS_CURRENT_HOST" ]; then
   sed "/DOMAIN_CURRENT_SITE/s/'[^']*'/'$WORDPRESS_CURRENT_HOST'/2" -i wp-config.php
 fi
 
+# DEV TRUE FALSE SED REPLACES DIFFERENT THAN STRING FORM ABOVE
 if [ "$WORDPRESS_DEV" == "true" ]; then
   printf "=> Disabling PHP OPCACHE \n"
   # php fpm cache gets triggered by opcache.enable_cli=1
   rm /usr/local/etc/php/conf.d/opcache-recommended.ini || true
 
-  sed "/WP_DEBUG/s/'[^']*'/'true'/2" -i wp-config.php
-  sed "/WP_DEBUG_DISPLAY/s/'[^']*'/'true'/2" -i wp-config.php
-    # show debug/error output
-  sed "/WP_DEBUG_LOG/s/'[^']*'/'true'/2" -i wp-config.php
+  sed "/'WP_DEBUG'/s/false/true/g" -i wp-config.php
+  sed "/'WP_DEBUG_LOG'/s/false/true/g" -i wp-config.php
+  # show debug/error output
+  sed "/'WP_DEBUG_DISPLAY'/s/true/false/g" -i wp-config.php
 else
-  sed "/WP_DEBUG/s/'[^']*'/'true'/2" -i wp-config.php
-  sed "/WP_DEBUG_LOG/s/'[^']*'/'true'/2" -i wp-config.php
+  sed "/'WP_DEBUG'/s/false/true/g" -i wp-config.php
+  sed "/'WP_DEBUG_LOG'/s/false/true/g" -i wp-config.php
   # just disable debug/error output
-  sed "/WP_DEBUG_DISPLAY/s/'[^']*'/'false'/2" -i wp-config.php
+  sed "/'WP_DEBUG_DISPLAY'/s/true/false/g" -i wp-config.php
 fi
